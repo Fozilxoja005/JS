fa0d5b876b6b59b92cdd9daebef597c8e8beeea5
Fozilxoja <fozil@gmail.com>
2022-03-26 14:09:52 +0500

    JAVA

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-// var name = 'Hello world'
-// var son = 23
-// var rost = true
-// console.log(name);
-// console.log(son);
-// console.log(rost);
-
-//  ism ='hello max'
-//  son = 56
-// console.log(ism);
-// console.log(son);
-
-
-// var a = '132'
-// var b = '456'
-// console.log(a * b)
- 
-// let ism = 'Mening ismim Fozilxoja yoshim '
-// let da = 'da '
-// let yosh = 19
-// console.log(ism+yosh+da);
-
-let ism = 'Mening ismim '
-let yourName = prompt('Sizning ismingiz nma ? ')
-let yil = prompt('hozir nichinchi yil ? ')
-let yilim = prompt('Siz nechinchi yilsiz ? ')
-let yosh = ' yoshim '
-let da = ' da '
-console.log(ism + yourName);
-alert(ism + yourName +yosh+(yil-yilim)+da)
-alert()
-
-
+let yourName = prompt('15*2/3+45-12')
+let misol = 15*2/3+45-12
+let javob = 'sizning javobingiz '
+console.log(javob + yourName);
+let javobb = 'togri javob '
+console.log(javobb + misol)
+let Yourname = prompt('15+5-10*24')
+let Misol = 15+5-10*24
+let Javob = 'sizning javobingiz '
+console.log(javob + Yourname);
+let Javobb = 'togri javob '
+console.log(javobb + Misol)
+let yourname = prompt('15*2/3+45-12')
+let mIsol = 15*2/3+45-12
+let jAvob = 'sizning javobingiz '
+console.log(jAvob + yourname);
+let jAvobb = 'togri javob '
+console.log(jAvobb + mIsol)
+let yOurame = prompt('80-20+10*2/4')
+let miSol = 80-20+10*2/4
+let jaVob = 'sizning javobingiz '
+console.log(jaVob + yOurame);
+let jaVobb = 'togri javob '
+console.log(jaVobb + miSol)
+let youRName = prompt('90/3+15*2-4')
+let misOl = 90/3+15*2-4
+let javOb = 'sizning javobingiz '
+console.log(javOb + youRName);
+let javObb = 'togri javob '
+console.log(javObb + misOl)
+let yourNAme = prompt('70-30/2*3+14')
+let misoL = 70-30/2*3+14
+let javoB = 'sizning javobingiz '
+console.log(javoB + yourNAme);
+let javoBb = 'togri javob '
+console.log(javoBb + misoL)
+let yourNamE = prompt('45*2/3+30-12')
+let misoll = 45*2/3+30-12
+let jaavob = 'sizning javobingiz '
+console.log(jaavob + yourNamE);
+let javobB = 'togri javob '
+console.log(javobB + misoll)
