09765d627145d128312f5eab274aa88cbd193b5e
Fozilxoja <fozilxoja@gmail.com>
2022-03-26 09:38:33 +0500

    js

---
+++ script.js
@@ -0,0 +1,33 @@
+// var name = 'Hello world'
+// var son = 23
+// var rost = true
+// console.log(name);
+// console.log(son);
+// console.log(rost);
+
+//  ism ='hello max'
+//  son = 56
+// console.log(ism);
+// console.log(son);
+
+
+// var a = '132'
+// var b = '456'
+// console.log(a * b)
+ 
+// let ism = 'Mening ismim Fozilxoja yoshim '
+// let da = 'da '
+// let yosh = 19
+// console.log(ism+yosh+da);
+
+let ism = 'Mening ismim '
+let yourName = prompt('Sizning ismingiz nma ? ')
+let yil = prompt('hozir nichinchi yil ? ')
+let yilim = prompt('Siz nechinchi yilsiz ? ')
+let yosh = ' yoshim '
+let da = ' da '
+console.log(ism + yourName);
+alert(ism + yourName +yosh+(yil-yilim)+da)
+alert()
+
+
